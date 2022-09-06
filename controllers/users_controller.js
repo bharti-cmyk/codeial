0807876d5b8aac793e37149f799eabf43c253480
bharti-cmyk <71355458+bharti-cmyk@users.noreply.github.com>
2022-09-06 16:19:05 +0530

    Second commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,20 @@ module.exports.profile = function(req, res){
         title: 'User Profile'
     })
 }
+
+exports.signUp =  function(req, res){
+    return res.render('user_sign_up', {title:"Codeial | Sign Up"});
+}
+
+exports.signIn = function(req,res){
+    return res.render('user_sign_in', {title:"Codeial | Sign In"});
+}
+
+exports.create = function(req,res){
+    //Todo
+}
+
+exports.createSession = function(req,res){
+    //Todo
+    
+}
